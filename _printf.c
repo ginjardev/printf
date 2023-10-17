29a7f5482a7be476fe6826bb27fcb22d0d71b7e2
Ini <arthurshur@gmail.com>
2023-10-17 05:13:08 -0700

    test

--- _printf.c
+++ _printf.c
@@ -8,24 +8,24 @@
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	unsigned int i, result;
+	unsigned int i, count;
 
-	result = 0;
+	count = 0;
 	va_start(ap, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
-			result += print_option(format[i + 1], ap);
+			count += print_option(format[i + 1], ap);
 			i++;
 		}
 		else
 		{
 			_putchar(format[i]);
-			result++;
+			count++;
 		}
 	}
 	va_end(ap);
-	return (result);
+	return (count);
 }

0660d98611420c74b08f089ba9818f13de266c79
Ini <arthurshur@gmail.com>
2023-10-18 14:22:38 -0700

    specifier for rot13

--- _printf.c
+++ _printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 		{'X', print_hexcap_arg}, {'o', print_oct_arg},
 		{'b', print_bin_arg}, {'p', print_address_arg},
 		{'r', print_str_rev}, {'S', print_custom_str},
-		{'\0', NULL},
+		{'R', print_rot13_arg}, {'\0', NULL},
 	};
 
 	if (format == NULL)
--- arg_functions2.c
+++ arg_functions2.c
@@ -40,3 +40,44 @@ int print_address_arg(va_list ap)
 	return (print_address(va_arg(ap, void *)));
 }
 
+/**
+ * print_rot13_arg - prints ROT13 coded string
+ * @ap: va_list object
+ * Return: number of characters printed
+ */
+int print_rot13_arg(va_list ap)
+{
+	char *str = va_arg(ap, char *);
+	int a, b;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char beta[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	if (str == NULL)
+	{
+		str = "(null)";
+
+		for (a = 0; str[a]; a++)
+			_putchar(str[a]);
+
+		return (a);
+	}
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if ((str[a] >= 'a' && str[a] <= 'z') || (str[a] >= 'A' && str[a] <= 'Z'))
+		{
+			for (b = 0; beta[b] != '\0'; b++)
+			{
+				if (str[a] == alpha[b])
+				{
+					_putchar(beta[b]);
+					break;
+				}
+			}
+		}
+		else
+			_putchar(str[a]);
+	}
+	return (a);
+}
+
